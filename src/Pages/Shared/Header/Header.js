1d68872dbe3a9844625056ad05c67b643bd1193c
Rafsan12 <rafsan.web.2022@gmail.com>
2022-04-26 15:13:56 +0600

    responsive

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h2>
+                    this is header
+                </h2>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
